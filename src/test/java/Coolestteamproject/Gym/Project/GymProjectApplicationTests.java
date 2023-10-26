524db41a725306e99d02713f9d3d30bc5221743c
Martin <martin.brnak47@gmail.com>
2023-10-26 18:49:13 -0400

    Initialized a web app with spring.io

---
+++ src/test/java/Coolestteamproject/Gym/Project/GymProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package Coolestteamproject.Gym.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
