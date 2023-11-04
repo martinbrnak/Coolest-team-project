9494865bdac3856fe58dab4ed25aa8c029911621
MartinBrnak <martin.brnak47@gmail.com>
2023-11-04 16:13:55 -0400

    Added the basic mysql call to app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const mysql = require("mysql");
 
 const app = express();
 const bodyParser = require("body-parser");
-
+const connection = mysql.createConnection({
+  host: 'database-1.cprkugnwm195.us-east-2.rds.amazonaws.com',
+  user: 'admin',
+  password: 'martinbrnak'
+})
 const port = 3000;
 
 app.use(cors());
@@ -13,6 +18,11 @@ const routes = require("./routes/spotify.js");
 
 app.use("/spotify", routes);
 
+
+connection.connect();
+
+
+connection.end();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
