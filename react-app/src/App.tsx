c6b53956cca974a2acd13c326f871475f02f62ed
MartinBrnak <martin.brnak47@gmail.com>
2023-11-30 18:43:02 -0500

    added Auth0 library to json and the app

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -11,25 +11,35 @@ import ExercisesPage from './ExercisesPage.tsx';
 import HomePage from './HomePage.tsx';
 import PlansPage from './PlansPage.tsx';
 import ExerciseList from './Exercise.tsx';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 
 const App: React.FC = () => {
   return (
 
+
     <div className="app">
       <Router>
-        <div className="app">
-          <Sidebar />
-          <main className="main-content">
-            <Routes>
-              <Route path="/history" element={<HistoryPage />} />
-              <Route path="/plans" element={<PlansPage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/home" element={<HomePage />} />
-              <Route path="/exercises" element={<ExercisesPage />} />
-            </Routes>
-          </main>
-        </div>
+        <Auth0Provider
+          domain="dev-ra4mhrpcnf2e1xtu.us.auth0.com"
+          clientId="FZD4oquHkioFyUqUlUNV4CewMMEoWMlS"
+          authorizationParams={{
+            redirect_uri: "localhost:3000/home"
+          }}
+        >
+          <div className="app">
+            <Sidebar />
+            <main className="main-content">
+              <Routes>
+                <Route path="/history" element={<HistoryPage />} />
+                <Route path="/plans" element={<PlansPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/home" element={<HomePage />} />
+                <Route path="/exercises" element={<ExercisesPage />} />
+              </Routes>
+            </main>
+          </div>
+        </Auth0Provider>
       </Router>
 
     </div>
