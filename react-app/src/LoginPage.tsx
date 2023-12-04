a588a64101ba9cd1c0f79d2e11c62704fb2fed00
MartinBrnak <martin.brnak47@gmail.com>
2023-12-04 17:01:26 -0500

    added login redirect, deleted loginpage interface

--- react-app/src/LoginPage.tsx
+++ react-app/src/LoginPage.tsx
@@ -1,90 +1,18 @@
-import React, { useState } from 'react';
-import './LoginPage.css';
-import LoginButton from './LoginButton.tsx';
-import LogoutButton from './LogoutButton.tsx';
-
-interface LoginCredentials {
-  username: string;
-  password: string;
-}
+import React, { useEffect } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const LoginPage: React.FC = () => {
-  const [credentials, setCredentials] = useState<LoginCredentials>({
-    username: '',
-    password: '',
-  });
-  const [loading, setLoading] = useState(false);
+  const { loginWithRedirect } = useAuth0();
 
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setCredentials((prevCredentials) => ({ ...prevCredentials, [name]: value }));
-  };
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-
-    // Add your login logic here, e.g., send a request to your authentication API
-
-    // Display loading spinner while the request is being processed
-
-    setLoading(true);
-
-    try {
-      // Simulating a login request (replace with actual login logic)
-      // For demonstration purposes, this just logs the credentials to the console
-      console.log('Logging in with:', credentials);
-
-      // Simulating a delay (replace with actual API request)
-      await new Promise((resolve) => setTimeout(resolve, 2000));
-
-      // Resetting the form and loading state
-      setCredentials({ username: '', password: '' });
-    } catch (error) {
-      console.error('Error during login:', error);
-    } finally {
-      setLoading(false);
-    }
-  };
+  useEffect(() => {
+    // Redirect the user to the login page immediately
+    loginWithRedirect();
+  }, [loginWithRedirect]);
 
   return (
-
-    <div className="login-container">
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={credentials.username}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={credentials.password}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-        <button type="submit" disabled={loading}>
-          {loading ? 'Logging in...' : 'Login'}
-        </button>
-      </form>
-      <div>
-        <LoginButton />
-      </div>
-      <div>
-        <LogoutButton />
-      </div>
+    // You can optionally render some content or a loading message here
+    <div>
+      <p>Redirecting to login...</p>
     </div>
   );
 };
