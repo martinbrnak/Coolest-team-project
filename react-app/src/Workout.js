908db07dff6d3483416dd3b125e417ee09ac492a
Ben Pogacar <bpogacar@bu.edu>
2023-12-07 16:52:18 -0500

    Workout.js completed logic

--- react-app/src/Workout.js
+++ react-app/src/Workout.js
@@ -2,7 +2,8 @@
 
 const express = require('express');
 const mysql = require('mysql');
-// const axios = require('axios');
+const cors = require('cors');
+
 
 const connection = mysql.createConnection({
     host: 'database-1.cprkugnwm195.us-east-2.rds.amazonaws.com',
@@ -13,9 +14,12 @@ const connection = mysql.createConnection({
 
 
 const workoutRouter = express.Router();
+workoutRouter.use(cors());
+workoutRouter.use(express.json());
 
-// to be able to get all workouts tied to a specific userID
-workoutRouter.get('/', (req, res, next) => {
+
+// to be able to get all workouts tied to a specific user for the history page
+workoutRouter.get('/:email', (req, res, next) => {
     connection.connect((err) => {
         if (err) {
             console.error('Error connecting to the database:', err.stack);
@@ -25,33 +29,56 @@ workoutRouter.get('/', (req, res, next) => {
         console.log('Connected to the database');
     })
 
-    const userEmail = req.body.email
-
-    if (foundWorkout) {
-        res.send(foundWorkout);
-    } else {
-        res.status(404).send();
-    }
+    const userEmail = req.params.email
+    const selectQuery = 'SELECT exercise, date FROM workouts WHERE user = ?';
+    
+    connection.query(selectQuery, [userEmail], (err, results, fields) => {
+        if (err) {
+            res.status(500).send();
+            return;
+        }
+        
+        if (results.length > 0) {
+            res.send(results);
+        } else {
+            res.status(404).send();
+        }
+    })
 });
 
 
 // posts a workout to the database
 workoutRouter.post('/', (req, res, next) => {
-    const receivedWorkout = req.query;
-    if (receivedWorkout) {
-        const insertQuery = 'INSERT INTO workouts SET ?';
-        connection.query(insertQuery, receivedWorkout, (error, results, fields) => {
-            if (error) {
-              console.error('Error inserting data:', error);
-              throw error;
-            }
-          
-            console.log('Data inserted successfully. Insert ID:', results.insertId);
-          });  
-        res.status(201).send(receivedWorkout);
-    } else {
-        res.status(400).send();
+    connection.connect((err) => {
+        if (err) {
+            console.error('Error connecting to the database:', err.stack);
+            return res.status(500).send();
+        }
+    
+        console.log('Connected to the database');
+    })
+
+    // req.body should be JSON.stringify({ user, exercise <-- name of exercise that they clicked })
+    const workout = {
+        user: req.body.user.email,
+        exercise: req.body.exercise
     }
+
+    const insertQuery = 'INSERT INTO workouts (user, exercise, date) VALUES (?, ?, CURRENT_DATE())';
+
+    connection.query(insertQuery, workout, (error, results, fields) => {
+        if (error) {
+        console.error('Error inserting data:', error);
+        return res.status(500).send();
+        }
+    
+        if (results && results.affectedRows > 0) {
+            res.status(201).send();
+        } else {
+            res.status(404).send('Workout not inserted.');
+        }
+    });  
+    
 });
 
 
