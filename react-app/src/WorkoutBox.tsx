86ab792d45d97e9bb36249ba2c54a787d949027e
MartinBrnak <martin.brnak47@gmail.com>
2023-12-08 15:25:43 -0500

    deleted useless import

--- react-app/src/WorkoutBox.tsx
+++ react-app/src/WorkoutBox.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth0 } from "@auth0/auth0-react";
-import { Navigate } from 'react-router-dom';
 
 const WorkoutBox: React.FC = () => {
   const { isAuthenticated, loginWithRedirect } = useAuth0();
